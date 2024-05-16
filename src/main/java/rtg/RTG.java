b22f829e2d7ff52583b8711bf9cb130fdc3eb906
Caedis <Caedis@users.noreply.github.com>
2024-05-16 16:31:45 -0500

    Remove fancy crash screen

--- src/main/java/rtg/RTG.java
+++ src/main/java/rtg/RTG.java
@@ -98,9 +98,7 @@ public class RTG {
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         if (Loader.isModLoaded("RWG")) {
-            if (event.getSide()
-                .isClient()) throw new IncompatibleModException("Realistic World Gen is incompatible with RTG");
-            else throw new RuntimeException("Realistic World Gen is incompatible with RTG");
+            throw new RuntimeException("Realistic World Gen is incompatible with RTG");
         }
 
         instance = this;
